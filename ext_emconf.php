5dad4ccac02a57a425b866101a6a0212303ac98a
o-ba <bo@cedev.de>
2020-04-13 13:14:43 +0200

    [TASK] Set 10.3 as the only compatible version

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '10.3.*',
+            'typo3' => '10.3.0',
             'php' => '7.2.0-7.4.99'
         ],
         'conflicts' => [],
